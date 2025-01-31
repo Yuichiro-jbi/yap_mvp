818f2397013f9a9db2022a27d29b051acb634d6d
Yuichiro-jbi <yoshioka-yuichiro@jbi.co.jp>
2025-01-31 10:07:02 +0900

    feat: 問題モデルの追加

---
+++ lib/model/question.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'question.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$QuestionImpl _$$QuestionImplFromJson(Map<String, dynamic> json) =>
+    _$QuestionImpl(
+      id: json['id'] as String,
+      question: json['question'] as String,
+      options:
+          (json['options'] as List<dynamic>).map((e) => e as String).toList(),
+      correctAnswerIndex: (json['correctAnswerIndex'] as num).toInt(),
+      explanation: json['explanation'] as String,
+    );
+
+Map<String, dynamic> _$$QuestionImplToJson(_$QuestionImpl instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'question': instance.question,
+      'options': instance.options,
+      'correctAnswerIndex': instance.correctAnswerIndex,
+      'explanation': instance.explanation,
+    };
